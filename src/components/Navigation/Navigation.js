88f0ef3e42d9175dc80148ef707f1e593c077883
sdbridges <sdbridges88@gmail.com>
2021-02-11 00:24:12 -0500

    Initial commit

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Navigation.css';
+
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+    if (isSignedIn) {
+      return (
+        <div>
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+          <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+        </nav>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+          <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+          <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
+        </nav>
+        </div>
+      );
+    }
+}
+
+export default Navigation;
